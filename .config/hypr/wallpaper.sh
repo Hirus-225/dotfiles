6f30fad889ba57b77e837a705245490adf7e5b96
Hirus-225 <czogoue@gmail.com>
2026-03-27 19:45:19 +0000

    actualisation swww

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -3,7 +3,8 @@
 DIR="/home/zogoue_charles/Images/Wallpaper"
 # Sélectionne une image au hasard
 PICS=($DIR/*)
-RANDOM_PIC=${PICS[$RANDOM % ${#PICS[@]}]}
+RANDOM_PIC=${PICS[$RANDOM%${#PICS[@]}]}
 
 # Applique l'image avec une transition aléatoire
-swww img "$RANDOM_PIC" --transition-type random --transition-step 90 
+swww img "$RANDOM_PIC" --transition-type random --transition-step 90
+
