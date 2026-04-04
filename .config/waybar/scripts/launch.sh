4630ee63e41dc9c47d35e4e392d0da89a27408c0
Hirus-225 <czogoue@gmail.com>
2026-04-04 11:19:26 +0000

    Actualisation

--- .config/waybar/scripts/launch.sh
+++ .config/waybar/scripts/launch.sh
@@ -6,7 +6,3 @@ waybar &
 
 # 2. On attend que Waybar soit totalement stable (3 secondes)
 sleep 3
-
-# 3. On lance 1Password en mode "silencieux" (dans le tray)
-# L'option --silent permet de ne pas ouvrir la fenêtre principale
-1password --silent &
