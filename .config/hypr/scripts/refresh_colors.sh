bf1358153171dd7fd294a4e2e86dadaf99e60c77
Hirus-225 <czogoue@gmail.com>
2026-04-21 21:29:40 +0000

    amélioration hyprlock

--- .config/hypr/scripts/refresh_colors.sh
+++ .config/hypr/scripts/refresh_colors.sh
@@ -5,22 +5,33 @@ sleep 0.5
 
 # Vérifier si le fichier de couleurs de Pywal existe
 if [ -f ~/.cache/wal/colors ]; then
-    # Génération sécurisée du fichier pour Hyprland
+    # --- Génération pour Hyprland ---
     COLOR1=$(cat ~/.cache/wal/colors | sed -n '2p' | sed 's/#//')
     COLOR2=$(cat ~/.cache/wal/colors | sed -n '3p' | sed 's/#//')
 
-    # On écrit le fichier proprement
     echo "\$color1 = rgb($COLOR1)" >~/.cache/wal/colors-hyprland.conf
     echo "\$color2 = rgb($COLOR2)" >>~/.cache/wal/colors-hyprland.conf
 
-    # Recharger Waybar
+    # --- Rechargement des composants UI ---
+
+    # 1. Waybar
     killall -SIGUSR2 waybar
 
-    # Recharger SwayNC
+    # 2. SwayNC (Notification Center)
     swaync-client -rs
 
-    # Forcer Hyprland à relire sa config pour effacer l'erreur rouge
+    # 3. SwayOSD (Le widget de volume/luminosité)
+    # On tue le serveur actuel et on le relance pour forcer le refresh du CSS
+    pkill swayosd-server
+    swayosd-server &
+
+    sleep 3
+
+    # 4. Hyprland
     hyprctl reload
+
+    # Optionnel : Notification de succès
+    # notify-send "Système" "Couleurs mises à jour avec succès" -i "color-management"
 else
     notify-send "Erreur" "Pywal n'a pas encore généré les couleurs."
 fi
